afce4791b8930b75969a0ead77b8e01a33346a92
Simone Vortice <simone.vortice@proton.me>
2024-04-22 13:02:56 +0200

    custom minimizer + minuit

--- fit.py
+++ fit.py
@@ -1,67 +1,63 @@
 import numpy as np
-from scipy.optimize import least_squares, curve_fit
+from scipy.optimize import least_squares
 from scipy.stats import chi2
-from utils import get_impedance_data
-from plot import plot_impedance_fit, plot_bodeplot
-import logging
-import glob
-
-logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-    filename='logs/main.log', filemode='w'
-)
 
 
-def residuals(params, x, data, model) -> np.ndarray:
+def residuals(params, x, data, model):
     y_model = model.func(params, x)
     res_real = y_model.real - data.real
     res_imag = y_model.imag - data.imag
-    res_mod = np.abs(y_model) - np.abs(data)
-    res_phase = np.angle(y_model) - np.angle(data)
-    return np.array([res_real, res_imag, res_mod, res_phase]).flatten()
+    return np.array([res_real, res_imag]).flatten()
 
 
 def loss(params, x, data, model) -> np.ndarray:
     return residuals(params, x, data, model)
 
 
-def fit(params0, x, data, model):
+def fit_complex(params0, x, data, model):
     params_fit = least_squares(
         loss, params0, bounds=(0, np.inf), args=(x, data, model),
         method='trf', loss='linear', ftol=1e-8, gtol=1e-8, xtol=1e-8,
     )
     data_fit = model.func(params_fit.x, x)
-    data_real = data_fit.real
-    data_imag = data_fit.imag
-    data_mod = np.abs(data_fit)
-    data_phase = np.angle(data_fit)
-    data_fit = np.array([data_real, data_imag, data_mod, data_phase]).flatten()
     return params_fit, data_fit
 
 
-def fit_curve(params0, x, data, model):
-    data = np.array([data.real, data.imag]).flatten()
-    x = np.array([x, x]).flatten()
-    print(x.size)
-    print(data.size)
-    data_err = 0.001 * np.abs(data)
-    popt, pcov = curve_fit(
-        f=model.func_ind, xdata=x, ydata=data, sigma=data_err,
-        bounds=(0, np.inf), p0=params0
+def residuals_phase(params, x, data, model) -> np.ndarray:
+    y_model = model.func(params, x)
+    res_real = y_model.real - data.real
+    res_imag = y_model.imag - data.imag
+    res_phase = np.angle(y_model) - np.angle(data)
+    return np.array([res_real, res_imag, res_phase]).flatten()
+
+
+def loss_phase(params, x, data, model) -> np.ndarray:
+    return residuals_phase(params, x, data, model)
+
+
+def fit_complex_phase(params0, x, data, model):
+    params_fit = least_squares(
+        loss_phase, params0,
+        bounds=(0, np.inf),
+        args=(x, data, model),
+        method='trf',
+        loss='linear',
+        ftol=1e-8,
+        gtol=1e-8,
+        xtol=1e-8,
     )
-    perr = np.sqrt(np.diag(pcov))
-    data_fit = model.func(popt, x[:x.size // 2])
-    return popt, perr, data_fit
+    data_fit = model.func(params_fit.x, x)
+    return params_fit, data_fit
 
 
-def best_fit(x, data, model, convergence_threshold=100, sigma=0.1):
-    np.random.seed(32)
+def best_fit_complex(x, data, model, convergence_threshold=100, sigma=0.1):
+    np.random.seed(42)
     params0 = [1 for _ in range(model.params_num)]
-    best_params_fit, best_data_fit = fit(params0, x, data, model)
+    best_params_fit, best_data_fit = fit_complex_phase(params0, x, data, model)
     best_cost = np.inf
     convergence_counter = 0
     while convergence_counter < convergence_threshold:
-        params_fit, data_fit = fit(params0, x, data, model)
+        params_fit, data_fit = fit_complex(params0, x, data, model)
         params0 = params_fit.x * \
             (1 + np.random.uniform(low=-sigma, high=sigma, size=model.params_num))
         if params_fit.cost < best_cost:
@@ -74,112 +70,23 @@ def best_fit(x, data, model, convergence_threshold=100, sigma=0.1):
     return best_params_fit, best_data_fit
 
 
-def chi2_test_pvalue(data, data_fit, model, params_fit) -> float:
-    length = len(data)
-    mod_err = 0.001 * np.ones(length // 2)
-    phase_err = 0.1 * np.ones(length // 2)
-    err = np.array([mod_err, phase_err]).flatten()
-    chi2_val = np.sum((data - data_fit)**2 / err**2)
-    dof = len(data_fit) - model.params_num
+def chi2_test_pvalue(data, data_fit, model, params_fit):
+    data, data_fit = data[3:], data_fit[3:]
+    magnitude, magnitude_fit = np.abs(data), np.abs(data_fit)
+    phase, phase_fit = np.angle(data), np.angle(data_fit)
+    res = np.concatenate((magnitude - magnitude_fit, phase - phase_fit))
+    mag_err = 0.001 * np.abs(data)
+    phase_err = np.full_like(phase, 0.1)
+    err = np.concatenate((mag_err, phase_err))
+    chi2_val = np.sum(res**2 / err**2)
+    dof = len(data) * 2 - model.params_num
     p_value = chi2.sf(chi2_val, dof)
     return p_value
 
 
-def fit_diode(diode, date, exp_type, models, sigma=0.1, convergence_threshold=30):
-    csv_files = glob.glob(f"experiments/{diode}_{date}/{exp_type}/*.csv")
-    stats = {}
-    failures = {}
-    for csv_file in csv_files:
-        bias = csv_file.split('/')[-1].split('.')[0]
-        freq, Z, theta = get_impedance_data(csv_file)
-        if len(Z) < 5:
-            logging.info(
-                f"{diode} @ {bias} has insufficient data (< 5 points)"
-            )
-            continue
-        for model in models:
-            print(f"\n\nFitting {diode} @ {bias} with {model.name}\n")
-            params, fit = best_fit(
-                freq, Z,
-                model, sigma=sigma, convergence_threshold=convergence_threshold
-            )
-            length = len(freq)
-            fit_real = fit[:length]
-            fit_imag = fit[length:2 * length]
-            fit_mod = fit[2 * length:3 * length]
-            fit_phase = fit[3 * length:]
-            fit_mp = np.array([fit_mod, fit_phase]).flatten()
-            fit_complex = fit_real + fit_imag * 1j
-            data_mp = np.array([np.abs(Z), theta]).flatten()
-            chi2_pvalue = chi2_test_pvalue(data_mp, fit_mp, model, params)
-            logging.info(f"Fit failed: {params.message}")
-            if chi2_pvalue < 0.05:
-                logging.info(
-                    f"Chi2 test failed for {diode} @ {bias} with {model.name} with p-value: {chi2_pvalue}"
-                )
-            plot_impedance_fit(
-                freq, Z,
-                fit_complex, params, model,
-                title=f"{diode} @ {bias} - {model.name} fit",
-            )
-            plot_bodeplot(
-                freq, Z, theta,
-                fit_complex, params, model,
-                title=f"{diode} @ {bias} - {model.name} fit",
-            )
-            if params.success:
-                stats[(bias, model)] = [
-                    (name, param) for name, param in zip(model.params_names, params.x)
-                ]
-                print(f"Fit successful: {params.message}")
-            else:
-                print(f"Fit failed: {params.message}")
-                failures[(diode, bias, model.name)] = params.message
-    return stats, failures
-
-
-def fit_diode_curve(diode, date, exp_type, models, sigma=0.1, convergence_threshold=30):
-    csv_files = glob.glob(f"experiments/{diode}_{date}/{exp_type}/*.csv")
-    stats = {}
-    failures = {}
-    for csv_file in csv_files:
-        bias = csv_file.split('/')[-1].split('.')[0]
-        freq, Z, theta = get_impedance_data(csv_file)
-        if len(Z) < 5:
-            logging.info(
-                f"{diode} @ {bias} has insufficient data (< 5 points)"
-            )
-            continue
-        for model in models:
-            p0 = [1 for _ in range(model.params_num)]
-            print(f"\n\nFitting {diode} @ {bias} with {model.name}\n")
-            try:
-                popt, perr, data_fit = fit_curve(p0, freq, Z, model)
-            except Exception as e:
-                logging.error(
-                    f"Fit failed for {diode} @ {bias} with {model.name} with message: {e}")
-                continue
-            chi2_pvalue = chi2_test_pvalue(
-                np.array([np.abs(Z), theta]).flatten(),
-                np.array([np.abs(data_fit), np.angle(
-                    data_fit, deg=True)]).flatten(),
-                model, popt
-            )
-            if chi2_pvalue < 0.05:
-                logging.info(
-                    f"Chi2 test failed for {diode} @ {bias} with {model.name} with p-value: {chi2_pvalue}"
-                )
-            plot_impedance_fit(
-                freq, Z,
-                data_fit, popt, model,
-                title=f"{diode} @ {bias} - {model.name} fit",
-            )
-            plot_bodeplot(
-                freq, Z, theta,
-                data_fit, popt, model,
-                title=f"{diode} @ {bias} - {model.name} fit",
-            )
-            stats[(bias, model)] = [
-                (name, param) for name, param in zip(model.params_names, popt)
-            ]
-    return stats, failures
+def chi2_test_pvalue_phase(data, data_fit, model, params_fit):
+    res = residuals_phase(params_fit.x, data, data_fit, model)
+    chi2_val = np.sum(res**2)
+    dof = len(data) * 3 - model.params_num
+    p_value = 1 - chi2.cdf(chi2_val, dof)
+    return p_value
--- test_minuit.py
+++ test_minuit.py
@@ -6,6 +6,7 @@ import glob
 import numpy as np
 from numpy import cos, sin
 import matplotlib.pyplot as plt
+from fit import best_fit_complex
 
 
 
@@ -37,20 +38,30 @@ freq_flat = np.concatenate((freq, freq))
 plt.errorbar(
     Z_real, Z_imag,
     xerr=Z_real_err, yerr=Z_imag_err,
-    ecolor='k', elinewidth=0.5, capsize=2, fmt='none', zorder=1
+    ecolor='k', elinewidth=0.5, capsize=2, fmt='none', zorder=1,
+    label="Data"
 )
 print(Z.shape)
 print(Z_err.shape)
 
+params_fit, data_fit = best_fit_complex(freq, Z, model)
+params0 = tuple(params_fit.x)
+print(params0)
+
 ls = LeastSquares(freq_flat, Z_flat, Z_err, model_func)
-m = Minuit(ls, (1, 1, 1, 1, 1), name=model.params_names)
+m = Minuit(ls, params0, name=model.params_names)
 m.limits = [(0, None), (0, None), (0, None), (0, None), (0, None)]
 m.migrad()
+m.hesse()
+m.minos()
 print(m)
-m.migrad()
-print(m)
-m.migrad()
-print(m)
+new_params = m.values
+print(new_params)
+data_fit2 = model.func(new_params, freq)
+
+plt.plot(data_fit.real, data_fit.imag, label=model_name, zorder=2)
+plt.plot(data_fit2.real, data_fit2.imag, label=model_name + "minuit", zorder=3)
+plt.legend()
 
 plt.show()
 
